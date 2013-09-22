227b52636f7646fc382b27a73c7a055af57e0734
Thiago Cesar <cesarthiago0@gmail.com>
2013-09-22 16:03:51 -0300

    correcao da rotacao do personagem
    personagem nao rotaciona sozinho apos colidir com a bola

--- Pezinho/main.lua
+++ Pezinho/main.lua
@@ -5,6 +5,7 @@ physics.start()
 physics.setGravity(0, 10)
 local sprite = require("sprite")
 
+
 --physics.setDrawMode("hybrid")
 
 -- Variaveis
@@ -25,23 +26,22 @@ grass_bottom.x = larguraTela/2; grass_bottom.y = alturaTela-35;
 physics.addBody( grass_bottom, "static", { friction=100, bounce=0.3 } )
 
 
-
-
 -- Add Jogador		
 local player = display.newImage( "imagens/jogador.png" )
 physics.addBody(player , "dynamic", { friction=-500,density=4000, bounce=-110, radius= 40} ) 
+player.isFixedRotation=true
 player.x = 50
 player.y= 340
 
 --add bola
-local bola = display.newImage("imagens/bola.png")
+local bola = display.newImage("imagens/bola2.png")
 bola.x = display.contentWidth/2
-physics.addBody(bola,"dynamic", { friction=100, density= 10, bounce=.9, radius = 30  } )
+physics.addBody(bola,"dynamic", { friction=100, density= 10, bounce=.9, radius = 15  } )
 
 
 --paredes
 paredeEsquerda = display.newRect( 0, -200, 1, 800)
-paredeDireita = display.newRect(display.contentWidth,-150,10, 800)
+paredeDireita = display.newRect(display.contentWidth,-200,10, 800)
 teto = display.newRect(0,-250,display.contentWidth,1)
 
 physics.addBody(paredeEsquerda, "static", {bounce = 0.1 })
@@ -50,7 +50,7 @@ physics.addBody(teto, "static", {bounce = 0.5 })
 
 -- Exibe pontuação
 
-textScore = display.newText("Score: 0", 10 , 10, "Helvetica", 20)
+textScore = display.newText("0", 10 , 10, "Helvetica", 20)
 textScore:setReferencePoint(display.TopLeftReferencePoint)
 textScore:setTextColor(255,255,255)
 textScore.x = display.contentWidth - 310
@@ -64,13 +64,16 @@ textLives:setTextColor(255,255,255)
 textLives.x = display.contentWidth - 310
 textLives.y = display.screenOriginY + 70
 
-local function updateScore(num)
-	score= score + num
-	textScore.text = "Score " .. score
-	textScore:setReferencePoint(display.TopLeftReferencePoint)
-	textScore.x = display.contentWidth - 310
+local function updateScore(event)
+	textScore.text=tonumber(textScore.text) + 100
 end
 
+local function onCollision(event)
+	if(event.object1 =="bola" or event.object2 =="bola") then
+		updateScore()
+	end
+end
+	
 -- Add Botao Jump button
 local up = display.newImage ("imagens/btn_arrow.png")
 	up.x = 280; up.y = 480;
@@ -84,6 +87,7 @@ local botaoEsquerdo= display.newImage ("imagens/btn_arrow.png")
 -- Add botaoDireito
 	local botaoDireito = display.newImage ("imagens/btn_arrow.png")
 	botaoDireito.x = 120; botaoDireito.y = 482;
+
 -- Stop 
 local function stop (event)
 	if event.phase =="ended" then
@@ -103,7 +107,9 @@ function up:touch(event)
 	if event.phase == "began"  then
 		player:setLinearVelocity( 0, -200 )
 	end
+	
 end
+
 up:addEventListener("touch",up)
 
 -- seta esquerda, move jogador para esquerda
@@ -117,4 +123,5 @@ local function moveDireita (event)
 	motionX = velocidade;
 end
 botaoDireito:addEventListener("touch",moveDireita)
+
 
